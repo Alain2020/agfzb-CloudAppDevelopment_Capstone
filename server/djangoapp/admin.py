d2f3ddf74422a63d43837c6af7400606703a18fa
Alain2020 <favouralain@yahoo.com>
2024-02-09 07:20:16 -0500

    In this lab, I have created the car model and car make related models in your Django app.

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 1
 
-# Register your models here.
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['name', 'type', 'car_make', 'dealer_id', 'year']
+    list_display = ('name', 'car_make', 'type', 'year')
 
-# CarModelInline class
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'description')
 
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
